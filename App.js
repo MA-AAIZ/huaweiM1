94a6be5fc0162a1f0d404fbc4ed9e8a695c4d1d2
MA-AAIZ <maaiz.2018283@iit.ac.lk>
2020-08-14 12:22:15 +0530

    Updated Commit

---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React,{Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Platform
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Body from './components/Body';
+
+export class App extends Component {
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Body/>
+      </View>
+    );
+  }   
+}
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
