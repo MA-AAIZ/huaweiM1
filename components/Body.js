6ccccd5e5429ada58ea0499570cb7ce6df710d18
madura <madurah@hsenidmobile.com>
2020-08-17 09:14:08 +0530

    [bug fix] Refactoring on Body View.

--- components/Body.js
+++ components/Body.js
@@ -1,81 +1,61 @@
-import React,{Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Platform
-} from 'react-native';
-import PlayServices, { GooglePlayServicesStatus } from 'react-native-play-services';
+import React, {useEffect, useState} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import PlayServices from 'react-native-play-services';
 
 const status = PlayServices.checkPlayServicesStatus();
 const GoogleAPIAvailability = require('react-native-google-api-availability-bridge');
-const val = [0];
-statusValue = 5;
 
-
-GoogleAPIAvailability.checkGooglePlayServices((result) => {
-  if(result === 'success') {
-		statusValue = 10;
-	}
-	if(result === 'update') {
-    GoogleAPIAvailability.promptGooglePlayUpdate(false);
-    statusValue = 30;
-  }
-  if(result === 'mssing') {
-    GoogleAPIAvailability.promptGooglePlayUpdate(false);
-    statusValue = 20;
-	}
-});
-
-PlayServices.sync();
-
-(async () => {
-  
-  switch (status){
-    case PlayServices.GooglePlayServicesStatus.AVAILABLE:
-      statusValue = 10;
-      break;
-    case PlayServices.GooglePlayServicesStatus.GMS_DISABLED:
-      statusValue = 20;
-      break;  
-  }
-  
-})();
-
-
-class detectHmsOrGms extends Component{
+function Body() {
     //gmsAvailable = GooglePlayServicesUtil.getInstance().isGooglePlayServicesAvailable(mContext);
     //hmsAvailable = HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(mContext);
+    const [statusValue, setStatusValue] = useState(0);
 
-    render(){
-        return(
-            <View style={styles.container}>
+    useEffect(() => {
+        PlayServices.sync().then(r => alert("Sync"));
+        (async () => {
+
+            switch (status) {
+                case PlayServices.GooglePlayServicesStatus.AVAILABLE:
+                    setStatusValue(10);
+                    break;
+                case PlayServices.GooglePlayServicesStatus.GMS_DISABLED:
+                    setStatusValue(20);
+                    break;
+            }
+
+        })();
+        GoogleAPIAvailability.checkGooglePlayServices((result) => {
+            if (result === 'success') {
+                setStatusValue(10);
+            }
+            if (result === 'update') {
+                GoogleAPIAvailability.promptGooglePlayUpdate(false);
+                setStatusValue(30);
+            }
+            if (result === 'mssing') {
+                GoogleAPIAvailability.promptGooglePlayUpdate(false);
+                setStatusValue(20);
+            }
+        });
+    }, [])
+
+    return (
+        <View style={styles.container}>
             <Text>{Platform.OS === 'ios' ? 'Device is IOS' : 'Device is Andriod'}</Text>
-            <Text>{statusValue  == 10 ? 'true' : 'false'}</Text>
+            <Text>{statusValue == 10 ? 'true' : 'false'}</Text>
             <Text>{statusValue}</Text>
-            <Text>{}</Text>
-            
-            <StatusBar style="auto" />
-            <View>
-              <Text>
-              
-              </Text>
-            </View>
-          </View>
-        );
-    };
-  };
+        </View>
+    );
 
-  const styles = StyleSheet.create({
+};
+
+const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  });
+});
 
-  export default detectHmsOrGms;
+export default Body;
