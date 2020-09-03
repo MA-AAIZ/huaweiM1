7e098509fe24587fd002586df162fd9730006458
MA-AAIZ <maaiz.2018283@iit.ac.lk>
2020-09-03 19:11:26 +0530

    commiting hms/gms --version 01

---
+++ components/Check.js
@@ -0,0 +1,95 @@
+import React,{Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Platform
+} from 'react-native';
+import PlayServices, { GooglePlayServicesStatus } from 'react-native-play-services';
+import HasHms from 'react-native-has-hms';
+
+hasGoogleMobileServices = async () => {
+    const hasGMS = await HasHms.isGMSAvailable()
+    console.log("Is your device running Google Mobile Services?", hasGMS)
+    return hasGMS
+}
+ 
+hasHuaweiMobileServices = async () => {
+    const hasHMS = await HasHms.isHMSAvailable()
+    console.log("Is your device running Huawei Mobile Services?", hasHMS)
+    return hasHMS
+}
+
+const status = PlayServices.checkPlayServicesStatus();
+const GoogleAPIAvailability = require('react-native-google-api-availability-bridge');
+const val = [0];
+statusValue = 5;
+
+
+GoogleAPIAvailability.checkGooglePlayServices((result) => {
+  if(result === 'success') {
+		statusValue = 10;
+	}
+	if(result === 'update') {
+    GoogleAPIAvailability.promptGooglePlayUpdate(false);
+    statusValue = 30;
+  }
+  if(result === 'mssing') {
+    GoogleAPIAvailability.promptGooglePlayUpdate(false);
+    statusValue = 20;
+	}
+});
+
+PlayServices.sync();
+
+(async () => {
+  
+  switch (status){
+    case PlayServices.GooglePlayServicesStatus.AVAILABLE:
+      statusValue = 10;
+      break;
+    case PlayServices.GooglePlayServicesStatus.GMS_DISABLED:
+      statusValue = 20;
+      break;  
+  }
+  
+})();
+
+
+
+class detectHmsOrGms extends Component{
+    //gmsAvailable = GooglePlayServicesUtil.getInstance().isGooglePlayServicesAvailable(mContext);
+    //hmsAvailable = HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(mContext);
+
+    render(){
+        return(
+            <View style={styles.container}>
+            <Text>{Platform.OS === 'ios' ? 'Device is IOS' : 'Device is Andriod'}</Text>
+            <Text>{statusValue  == 10 ? 'true' : 'false'}</Text>
+            <Text>{statusValue}</Text>
+            <Text>{}</Text>
+            
+            <StatusBar style="auto" />
+            <View>
+              <Text>
+              
+              </Text>
+            </View>
+          </View>
+        );
+    };
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  export default detectHmsOrGms;
